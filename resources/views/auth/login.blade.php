727982c60e0218ea78dd09854dd08e4751c51852
imass02 <ambariimass@gmail.com>
2026-04-14 07:33:15 +0700

    hmm lagi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html class="h-100" lang="id">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Rosella - Login</title>
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin/assets/images/favicon.png') }}">
-    <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet">
-</head>
-<body class="h-100">
-    <!--*******************
-        Preloader start
-    ********************-->
-    <div id="preloader">
-        <div class="loader">
-            <svg class="circular" viewBox="25 25 50 50">
-                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3"
-                    stroke-miterlimit="10" />
-            </svg>
-        </div>
-    </div>
-    <!--*******************
-        Preloader end
-    ********************-->
-
-    <div class="login-form-bg h-100">
+@extends('layouts.app')
+@section('content')
+        <div class="login-form-bg h-100">
         <div class="container h-100">
             <div class="row justify-content-center h-100">
                 <div class="col-xl-6">
@@ -95,14 +70,4 @@
             </div>
         </div>
     </div>
-
-    <!--**********************************
-        Scripts
-    ***********************************-->
-    <script src="{{ asset('admin/assets/plugins/common/common.min.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/custom.min.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/settings.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/gleek.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/styleSwitcher.js') }}"></script>
-</body>
-</html>
+    @endsection
